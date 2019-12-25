f889bc22a8a84c9b03d7d6d73d1226fc9fba3f5a
hasefumi23 <hasefumi23@gmail.com>
2019-12-25 22:41:22 +0900

    [from now] 2019/12/25 22:41:22
    diff --git a/ekimei.ml b/ekimei.ml
    new file mode 100644
    index 0000000..266c565
    --- /dev/null
    +++ b/ekimei.ml
    @@ -0,0 +1,20 @@
    +(* 目的: 駅の情報を表す型 *)
    +type ekimei_t = {
    +  kanji : string; (* 漢字の駅名 *)
    +  kana : string; (* ひらがなの駅名 *)
    +  romaji : string; (* ローマ字の駅名 *)
    +  shozoku : string; (* 所属する路線名 *)
    +}
    +
    +(* 駅の情報 ekimei_t を受け取って情報を表示する *)
    +(* hyouji ekimei_t -> string *)
    +let hyouji ekimei = match ekimei with
    +  {kanji = kanji_a;
    +   kana = kana_a;
    +   romaji = romaji_a;
    +   shozoku = shozoku_a;
    +  } -> shozoku_a ^ "、" ^ kanji_a ^ "(" ^ kana_a ^ ")"
    +
    +(* テスト *)
    +let test1 = hyouji {kanji = "佐賀"; kana = "さが"; romaji = "saga"; shozoku = "佐賀線"} = "佐賀線、佐賀(さが)"
    +let test2 = hyouji {kanji = "福岡"; kana = "ふくおか"; romaji = "fukuoka"; shozoku = "九州線"} = "九州線、福岡(ふくおか)"

---
+++ ekimei.ml
@@ -0,0 +1,20 @@
+(* 目的: 駅の情報を表す型 *)
+type ekimei_t = {
+  kanji : string; (* 漢字の駅名 *)
+  kana : string; (* ひらがなの駅名 *)
+  romaji : string; (* ローマ字の駅名 *)
+  shozoku : string; (* 所属する路線名 *)
+}
+
+(* 駅の情報 ekimei_t を受け取って情報を表示する *)
+(* hyouji ekimei_t -> string *)
+let hyouji ekimei = match ekimei with
+  {kanji = kanji_a;
+   kana = kana_a;
+   romaji = romaji_a;
+   shozoku = shozoku_a;
+  } -> shozoku_a ^ "、" ^ kanji_a ^ "(" ^ kana_a ^ ")"
+
+(* テスト *)
+let test1 = hyouji {kanji = "佐賀"; kana = "さが"; romaji = "saga"; shozoku = "佐賀線"} = "佐賀線、佐賀(さが)"
+let test2 = hyouji {kanji = "福岡"; kana = "ふくおか"; romaji = "fukuoka"; shozoku = "九州線"} = "九州線、福岡(ふくおか)"
