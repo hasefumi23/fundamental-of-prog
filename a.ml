be5ab8ca043d5863c9d81f44735b3df350b0ec07
hasefumi23 <hasefumi23@gmail.com>
2020-01-04 00:45:30 +0900

    [from now] 2020/01/04 00:45:30
    diff --git a/a.ml b/a.ml
    new file mode 100644
    index 0000000..f9bbd78
    --- /dev/null
    +++ b/a.ml
    @@ -0,0 +1,11 @@
    +(* 目的: 漸化式で定義される数列 anの第n項を求める関数 *)
    +(* a : int -> int *)
    +let rec a n =
    +  if n = 0 then 3
    +  else 2 * a (n - 1) - 1
    +
    +(* テスト *)
    +let test1 = a 0 = 3
    +let test2 = a 1 = 5
    +let test3 = a 2 = 9
    +let test4 = a 3 = 17

---
+++ a.ml
@@ -0,0 +1,11 @@
+(* 目的: 漸化式で定義される数列 anの第n項を求める関数 *)
+(* a : int -> int *)
+let rec a n =
+  if n = 0 then 3
+  else 2 * a (n - 1) - 1
+
+(* テスト *)
+let test1 = a 0 = 3
+let test2 = a 1 = 5
+let test3 = a 2 = 9
+let test4 = a 3 = 17
