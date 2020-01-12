daaa741bc68540b59bbef72c44c2dc532653a8c2
hasefumi23 <hasefumi23@gmail.com>
2020-01-12 21:20:54 +0900

    [from now] 2020/01/12 21:20:53
    diff --git a/minimum.ml b/minimum.ml
    index 4580048..ced71ef 100644
    --- a/minimum.ml
    +++ b/minimum.ml
    @@ -1,15 +1,28 @@
     (* 目的: 受け取った lst の中の最小値を返す *)
     (* minimum int list -> int *)
    -let rec minimum lst = match lst with
    +(* let rec minimum lst = match lst with
         [] -> max_int
       | first :: rest ->
       let min_rest = minimum rest in
       if first <= min_rest
         then first
    -    else min_rest
    +    else min_rest *)
    +
    +let rec new_minimum f rest = match rest with
    +    [] -> f
    +  | first :: rest ->
    +  let min_rest = new_minimum first rest in
    +  if first <= min_rest
    +  then first
    +  else min_rest
    
     (* テスト *)
    -let test1 = minimum [3] = 3
    -let test2 = minimum [1; 2] = 1
    -let test3 = minimum [3; 2] = 2
    -let test4 = minimum [3; 2; 6; 4; 1; 8] = 1
    +(* let test1 = new_minimum [3] = 3
    +let test2 = new_minimum [1; 2] = 1
    +let test3 = new_minimum [3; 2] = 2
    +let test4 = new_minimum [3; 2; 6; 4; 1; 8] = 1 *)
    +
    +let test1 = new_minimum 3 [3] = 3
    +let test2 = new_minimum 1 [1; 2] = 1
    +let test3 = new_minimum 3 [3; 2] = 2
    +let test4 = new_minimum 3 [3; 2; 6; 4; 1; 8] = 1

--- minimum.ml
+++ minimum.ml
@@ -1,15 +1,28 @@
 (* 目的: 受け取った lst の中の最小値を返す *)
 (* minimum int list -> int *)
-let rec minimum lst = match lst with
+(* let rec minimum lst = match lst with
     [] -> max_int
   | first :: rest ->
   let min_rest = minimum rest in
   if first <= min_rest
     then first
-    else min_rest
+    else min_rest *)
+
+let rec new_minimum f rest = match rest with
+    [] -> f
+  | first :: rest ->
+  let min_rest = new_minimum first rest in
+  if first <= min_rest
+  then first
+  else min_rest
 
 (* テスト *)
-let test1 = minimum [3] = 3
-let test2 = minimum [1; 2] = 1
-let test3 = minimum [3; 2] = 2
-let test4 = minimum [3; 2; 6; 4; 1; 8] = 1
+(* let test1 = new_minimum [3] = 3
+let test2 = new_minimum [1; 2] = 1
+let test3 = new_minimum [3; 2] = 2
+let test4 = new_minimum [3; 2; 6; 4; 1; 8] = 1 *)
+
+let test1 = new_minimum 3 [3] = 3
+let test2 = new_minimum 1 [1; 2] = 1
+let test3 = new_minimum 3 [3; 2] = 2
+let test4 = new_minimum 3 [3; 2; 6; 4; 1; 8] = 1
